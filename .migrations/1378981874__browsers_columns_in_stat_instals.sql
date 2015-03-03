f9b518eb1832c2e046737d387ee4e24b154124c6
mgalex <mgalex8@yandex.ru>
2015-03-03 22:35:28 +0300

    Delete migrations

--- .migrations/1378981874__browsers_columns_in_stat_instals.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `stats_installs` CHANGE `amount_installs` `amount_installs_chrome` INT( 11 ) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `stats_installs` DROP `amount_uninstalls`;
-ALTER TABLE `stats_installs` ADD `amount_installs_firefox` INT UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `stats_installs` ADD `amount_installs_ie` INT UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `stats_installs` ADD `amount_installs_unknown` INT UNSIGNED NOT NULL DEFAULT '0';
